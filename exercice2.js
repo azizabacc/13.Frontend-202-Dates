129a9138d7007fa0b889cf43beac353b21ec9994
Aziza Baccouri <azizabaccouri@gmail.com>
2023-06-23 09:53:58 +0200

    exercice2

--- exercice2.js
+++ exercice2.js
@@ -2,16 +2,16 @@
 Write a function to find how many days have passed since any point in time (after 1970). */
 
 function calculateDaysSinceBirth(birthday) {
-    var currentTime = Date.now();
+    let currentTime = Date.now();
     console.log(currentTime);
-    var birthTime = new Date(birthday).getTime();
-    var timeDifference = currentTime - birthTime;
-    var daysPassed = Math.floor(timeDifference / (24 * 60 * 60 * 1000));
+    let birthTime = new Date(birthday).getTime();
+    let timeDifference = currentTime - birthTime;
+    let daysPassed = Math.floor(timeDifference / (24 * 60 * 60 * 1000));
     
     return daysPassed;
   }
   
-  var birthday= '2023-06-22';
-  var daysSinceBirth = calculateDaysSinceBirth(birthday);
+  let birthday= '2023-06-22';
+  let daysSinceBirth = calculateDaysSinceBirth(birthday);
   console.log('Days since my birth:', daysSinceBirth," days.");
   
